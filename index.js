fa04f370efd8574d9f479b300a007e454497f062
mac_itthikon <zero-no-connect@hotmail.com>
2025-11-09 20:54:37 +0700

    four commit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 const port = 2000;
 
 app.get('/',(req, res)=>{
     console.log(req.query);
     const { q, sortBy } = req.query;
-    res.send(`สวัสดีจ้าาา q=${q}, sortBy=${sortBy}`);
+    res.render('home', {q, sortBy});
+});
+
+app.get('/p/new', (req, res) =>{
+    res.render('postNew');
+});
+
+app.post('/p/new', (req, res) => {
+    console.log(req.body);
+    const { title } = req.body ?? {};
+    res.send(`Submit ฟอร์มแล้วจ้า title=${title}`);
+});
+
+
+app.get('/p/:postId', (req, res)=>{
+    console.log(req.params);
+    const { postId } = req.params;
+    res.render('postId', { postId });
 });
 
 app.listen(port, ()=> {
